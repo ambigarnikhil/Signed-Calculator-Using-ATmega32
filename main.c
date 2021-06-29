7bee04a3bb66a6e6ce74827a8f341cb7d4292168
Nikhil Ambigar <70022502+ambigarnikhil@users.noreply.github.com>
2021-06-29 11:44:39 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,32 @@
+/*
+ * main.c
+ *
+ * Created: 6/26/2021 10:18:31 PM
+ * Author : Nikhil
+ */
+
+#include "calc.h"   
+
+static long int a,b; 
+int main(void)
+{
+	
+	cal_init();
+	
+	dis_string(0,0,"When      Come    ;)");
+	dis_string(1,4,"Dreams    True ;)");
+	
+	_delay_ms(50);
+	
+	for (int start = 0;start < 25; ++start)
+	{
+		_delay_ms(5);
+		dis_shift('L');
+	}
+	
+	while(1)
+	{
+		calculate();
+	}
+}
+
